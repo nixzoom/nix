176f78ea671a86a2a1c094439c1c564d202cf971
Nix Zoom <nix.zoom@gmail.com>
2014-11-11 09:53:18 +0300

    Adding comments on ddos.sh

--- ddos.sh
+++ ddos.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#bipin added
+##Create backup directory
 iptables -I INPUT -p tcp --dport 25 -i eth0 -m state --state NEW -m recent --set
 iptables -I INPUT -p tcp --dport 25 -i eth0 -m state --state NEW -m recent --update --seconds 60 --hitcount 4 -j DROP
 
