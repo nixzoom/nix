52f1875bd5aeb57929baf95d9a16919b1c9bc323
Nix Zoom <nix.zoom@gmail.com>
2014-11-11 09:57:25 +0300

    Adding script datacenter.sh

---
+++ datacenter.sh
@@ -0,0 +1,13 @@
+# Get all domains
+_dom=$@
+ 
+# Die if no domains are given
+[ $# -eq 0 ] && { echo "Usage: $0 domain1.com domain2.com ..."; exit 1; }
+for d in $_dom
+do
+	_ip=$(host $d | grep 'has add' | head -1 | awk '{ print $4}')
+	[ "$_ip" == "" ] && { echo "Error: $d is not valid domain or dns error."; continue; }
+	echo "Getting information for domain: $d [ $_ip ]..."
+	whois "$_ip" | egrep -w 'OrgName:|City:|Country:|OriginAS:|NetRange:'
+	echo ""
+done
