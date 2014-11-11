33d67693c100307206484abedea9fd26f6b4afc6
Nix Zoom <nix.zoom@gmail.com>
2014-11-11 09:47:34 +0300

    Adding another script ddosh.sh

---
+++ ddos.sh
@@ -0,0 +1,3 @@
+iptables -I INPUT -p tcp --dport 25 -i eth0 -m state --state NEW -m recent --set
+iptables -I INPUT -p tcp --dport 25 -i eth0 -m state --state NEW -m recent --update --seconds 60 --hitcount 4 -j DROP
+
